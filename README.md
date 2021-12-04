e1eb1281ebf05e4c6e44d414e75e88966d3a1fb1
rainy-25Ghz <1748420480@qq.com>
2021-12-04 14:11:16 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-ohv6ex
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-ohv6ex)
---
+++ index.ts
@@ -0,0 +1,7 @@
+// Import stylesheets
+import './style.css';
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+const canvas = document.getElementById('canvas') as HTMLCanvasElement;
+console.log(canvas, appDiv);
